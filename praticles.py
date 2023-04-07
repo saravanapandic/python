8dc23468d7138131e362cfa696717768014c3ee8
saravanapandic <csaravanakd@gmail.com>
2023-04-07 21:18:21 +0530

    turtle

---
+++ praticles.py
@@ -0,0 +1,32 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.penup()
+t.goto(0, -100)
+t.pendown()
+t.circle(100)
+
+
+t.penup()
+t.goto(-50, 0)
+t.pendown()
+t.begin_fill()
+t.circle(20)
+t.end_fill()
+
+t.penup()
+t.goto(50, 0)
+t.pendown()
+t.begin_fill()
+t.circle(20)
+t.end_fill()
+
+t.penup()
+t.goto(-60, -40)
+t.pendown()
+t.right(45)
+t.circle(70, 90)
+t.hideturtle()
+turtle.done()
+a=10
